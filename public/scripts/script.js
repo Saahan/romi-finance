733b23f9c9338be8764154c4fd3d49ffcfda1e89
Saahan <smombasa1989@gmail.com>
2023-10-30 18:40:20 +0530

    update navbar and responsiveness of dashboard

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,8 @@
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChartSmall);
 google.charts.setOnLoadCallback(drawChart2);
+google.charts.setOnLoadCallback(drawChart2Small);
 
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
@@ -43,6 +45,52 @@ function drawChart2() {
 
   var chart = new google.visualization.PieChart(
     document.getElementById("piechart2")
+  );
+
+  chart.draw(data, options);
+}
+
+function drawChartSmall() {
+  var data = google.visualization.arrayToDataTable([
+    ["Status", "Percentage"],
+    ["Liquidity", 94],
+    ["Not Staked", 6],
+  ]);
+
+  var options = {
+    title: "Distribution",
+    width: 200,
+    height: 200,
+    legend: "none",
+    backgroundColor: "#151515",
+    title: "",
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart-small")
+  );
+
+  chart.draw(data, options);
+}
+
+function drawChart2Small() {
+  var data = google.visualization.arrayToDataTable([
+    ["Currency", "Percentage"],
+    ["USDC", 49.5],
+    ["ETH", 50.49],
+  ]);
+
+  var options = {
+    title: "Distribution",
+    width: 200,
+    height: 200,
+    legend: "none",
+    backgroundColor: "#151515",
+    title: "",
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart-small2")
   );
 
   chart.draw(data, options);
